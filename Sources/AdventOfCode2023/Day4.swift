147a95b1d8174ea2c498ea9e24194eed69c6d426
lukasneukom <6952622+lukasneukom@users.noreply.github.com>
2023-12-04 07:10:41 +0100

    Day 4 bare minimum

---
+++ Sources/AdventOfCode2023/Day4.swift
@@ -0,0 +1,87 @@
+//
+//  File.swift
+//  
+//
+//  Created by Lukas Neukom on 04.12.2023.
+//
+
+import Foundation
+
+import AdventOfCodeCore
+
+public class Day4: AOCDay {
+    public let puzzleInput: String?
+    
+    public init(puzzleInput input: String?) {
+        puzzleInput = input ?? ""
+    }
+
+    public func solvePart1() -> String {
+        let cards = puzzleInput!.components(separatedBy: .newlines)
+        
+        var sumOfPoints = 0
+        var scratchcards = [Int: Int]()
+        var sumOfCards = 0
+        
+        for i in 0..<cards.count {
+            scratchcards[i] = 1
+        }
+        print(scratchcards)
+        
+        var cardCount = 0
+        
+        for card in cards {
+            print("reading card \(cardCount)")
+            let numbers = card.components(separatedBy: ":")
+            let numberLists = numbers[1].components(separatedBy: " | ")
+            let winning = numberLists[0].components(separatedBy: .whitespaces)
+            let chosen = numberLists[1].components(separatedBy: .whitespaces)
+            
+            var winningNumbers = Set<Int>()
+            var points = 0
+            var matches = 0
+            var ownCards = scratchcards[cardCount]!
+            
+            for winner in winning {
+                if winner != "" {
+                    winningNumbers.insert(Int(winner)!)
+                }
+            }
+            
+            for choice in chosen {
+                if choice != "" {
+                    if winningNumbers.contains(Int(choice)!) {
+                        matches += 1
+                        if points == 0 {
+                            points = 1
+                        } else {
+                            points *= 2
+                        }
+                    }
+                }
+            }
+            
+            sumOfPoints += points
+            
+            if points > 0 {
+                print("owning \(ownCards), adding to next \(matches)")
+                for i in 1...matches {
+                    let newV = scratchcards[cardCount+i]! + 1*ownCards
+                    scratchcards[cardCount+i] = newV
+                }
+            }
+            print(scratchcards)
+            cardCount += 1
+        }
+        
+        for (number, amount) in scratchcards {
+            sumOfCards += amount
+        }
+        
+        print(sumOfPoints)
+        print(sumOfCards)
+        print("")
+        
+        return "\(sumOfPoints)"
+    }
+}
